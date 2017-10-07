9ba615464f773bfbe1ec3a6b901d95174ed8906d
SolidZORO <SolidZORO@live.com>
2017-10-08 02:23:30 +0800

    dp

--- _statics/____scripts/app_devvvvvvvvvvvvvvvv.js
+++ _statics/____scripts/app_devvvvvvvvvvvvvvvv.js
@@ -1,4 +1,4 @@
-/*! ver. 1507175419244 - SolidZORO <solidzoro@live.com> */
+/*! ver. 1507391717589 - SolidZORO <solidzoro@live.com> */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -58,24 +58,38 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://otv0i859b.bkt.clouddn.com/_statics";
+/******/ 	__webpack_require__.p = "http://solidzoro.xxx/_statics";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports) {
+
+module.exports = bundle_5e40794c63791c037a30;
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(1);
+var _jquery = __webpack_require__(2);
+
+var _jquery2 = _interopRequireDefault(_jquery);
 
 __webpack_require__(3);
 
-var clearCache = '2017-10-04';
+__webpack_require__(4);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+window.$ = _jquery2.default;
+window.jquery = _jquery2.default;
+window.jQuery = _jquery2.default;
 
 document.addEventListener('DOMContentLoaded', function () {
     if (/solidzoro\.com/.test(window.location.origin) === false) {
@@ -84,28 +98,49 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 }, false);
 
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?63392a0ec51d4ad23453b82650af1329";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
+console.log(333);
 
-/***/ }),
-/* 1 */
-/***/ (function(module, exports, __webpack_require__) {
+(0, _jquery2.default)(function () {
+    if ((0, _jquery2.default)('.layout-body--book').length) {
+        console.log('is-layout-body--book');
 
-module.exports = (__webpack_require__(2))(1);
+        _jquery2.default.ajax({
+            type: 'get',
+            async: false,
+            url: 'https://api.douban.com/v2/book/user/SolidZORO/collections',
+            dataType: 'jsonp',
+            jsonp: 'callback',
+            jsonpCallback: 'bookList',
+            success: function success(jsonp) {
+                var bookListEl = (0, _jquery2.default)('.book-list');
+                bookListEl.html('');
+
+                _jquery2.default.each(jsonp.collections, function (index, value) {
+                    console.log(value);
+                    bookListEl.append('\n                        <li class="book-list__item">\n                            <div class="image">\n                                <a href="' + value.book.alt + '" target=_blank><img src="' + value.book.images.large + '" alt="' + value.book.title + '"></a>\n                            </div>\n                            <div class="info">\n                                <h2 class="title">\n                                    <a href="' + value.book.alt + '" target=_blank>' + value.book.title + '</a>\n                                </h2>\n                                <h3 class="author">' + value.book.author + '</h3>\n                                <p class="comment">' + (value.comment ? value.comment : 'N/A') + '</p>\n                            </div>\n                        </li>\n                    ');
+                });
+            },
+            error: function error() {
+                console.log('fail');
+            }
+        });
+    }
+});
 
 /***/ }),
 /* 2 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-module.exports = bundle_1c7c3f170022d8e78f45;
+module.exports = (__webpack_require__(0))(3);
 
 /***/ }),
 /* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(0))(1);
+
+/***/ }),
+/* 4 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
